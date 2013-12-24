28c50f2357bf9c91a238226acd6ab95c3796c25b
vagrant <vagrant@precise64.(none)>
2013-12-24 09:55:20 +0000

    Change Koa app.js for static file serve, routing, template

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var koa = require('koa');
+var route = require('koa-route');
+var serve = require('koa-static');
+var views = require('co-views');
 var app = koa();
 
-// logger
+var render = views(__dirname + '/views', { map : {html : 'jade'}});
 
-app.use(function *(next){
-  var start = new Date;
-  yield next;
-  var ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
-});
+// GET /views => render template engine
+app.use(route.get('/views', function *(next) {
+  this.body = yield render('index.jade', {name: "koa"});
+}));
 
-// response
+// GET /hello => 'Hello!'
+app.use(route.get('/hello', function *() {
+  this.body = 'Hello!!';
+}));
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+// GET /hello/:name => 'Hello :name'
+app.use(route.get('/hello/:name', function *(name) {
+  this.body = 'Hello ' + name;
+}));
+
+
+// static file serv
+app.use(serve(__dirname + '/public'));
 
 app.listen(3000);
